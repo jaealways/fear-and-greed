7327c909cecaf82bc4c9fb09eff90964c1b10620
jaealways <71856506+jaealways@users.noreply.github.com>
2022-01-25 20:02:50 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,14 +1,13 @@
-from score import VolatilityVolume
-from calculation import VariableCalculation
+from score import VolatilityVolume, Momentum
 
 import numpy as np
+import pandas as pd
 
+# 어떤 지점에서 nan으로 치환??
 
-a = np.array([[3, 1], [2, np.nan]])
-b = np.array([[4, 1], [2, np.nan]])
+df_mcpi_volume = pd.read_pickle("df_song_volume_17-01-01_23-12-31.pkl")
+df_mcpi_index = pd.read_pickle("df_price_17-01-01_23-12-31.pkl")
+mcpi_volume, mcpi_index = df_mcpi_volume.to_numpy(), df_mcpi_index.iloc[:,:-1].to_numpy()
 
-s = VariableCalculation().variance_price(a)
-s
-
-# S_vv = VolatilityVolume(a, b)
-
+score_vv = Momentum(mcpi_index, mcpi_volume).compute_compensation()
+score_vv
