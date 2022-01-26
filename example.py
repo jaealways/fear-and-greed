1fb91c9fc61c254537bc8a73c86775bdceaee792
jaealways <71856506+jaealways@users.noreply.github.com>
2022-01-26 19:38:10 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from score import VolatilityVolume, Momentum
+from score import Score, FearGreed
 
 import numpy as np
 import pandas as pd
@@ -7,7 +7,8 @@ import pandas as pd
 
 df_mcpi_volume = pd.read_pickle("df_song_volume_17-01-01_23-12-31.pkl")
 df_mcpi_index = pd.read_pickle("df_price_17-01-01_23-12-31.pkl")
-mcpi_volume, mcpi_index = df_mcpi_volume.to_numpy(), df_mcpi_index.iloc[:,:-1].to_numpy()
+x, y = df_mcpi_index.to_numpy(), df_mcpi_volume.to_numpy()
 
-score_vv = Momentum(mcpi_index, mcpi_volume).compute_compensation()
-score_vv
+score = Score(x, y)
+score_fng = FearGreed(x, y, score).compute_grad()
+score_fng
